1860d519a8595b57c4660097e77dd44fc17f9e75
WGuandique-2005 <wguandiquerivera07@gmail.com>
2024-10-05 16:30:07 -0600

    commit

--- EJERCICIOS/pie_graph.py
+++ EJERCICIOS/pie_graph.py
@@ -1,34 +1,39 @@
 """
-* Haciendo uso de la página de Kaggle, seleccionar tres diferentes datasets.
-! c. Un gráfico circular
-Enlace al dataset utilizado: https://www.kaggle.com/datasets/imtkaggleteam/netflix
+__________________________________________________________________________
+Haciendo uso de la página de Kaggle, seleccionar tres diferentes datasets.
+    C) Un gráfico circular
+Enlace al dataset utilizado: 
+https://www.kaggle.com/datasets/mohitkumar282/leetcode-questions-dataset
+__________________________________________________________________________
 """
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar el dataset de NetFlix.csv
-netflix_data = pd.read_csv('dataset/NetFlix.csv')
+# Cargamos el dataset 
+datos = pd.read_csv('dataset/Leetcode_Questions.csv')
 
-# Contar la frecuencia de cada tipo (movie o TV show)
-type_counts = netflix_data['type'].value_counts()
+# Contamos la frecuencia 
+Difficulty_counts = datos['Difficulty'].value_counts()
 
-# Crear el gráfico de pastel
+# Creamos el gráfico de pastel
 plt.figure(figsize=(10,8))
-plt.pie(type_counts, labels = type_counts.index, autopct='%1.1f%%', 
-        colors=['#3498db', '#e74c3c'],
-        startangle=90,  # Iniciar el gráfico desde arriba
-        explode=[0.05, 0.05],  # Separar un poco cada sección del centro
+plt.pie(Difficulty_counts, labels = Difficulty_counts.index, autopct='%1.1f%%', 
+        colors=['pink', 'green', 'yellow'],
+        startangle=90,  # Iniciamos el gráfico desde arriba
+        explode=[0.05, 0.05, 0.05],  # Separamos un poco cada sección del centro
         pctdistance=0.85,  # Distancia entre el porcentaje y el centro
         labeldistance=1.1,  # Distancia entre la etiqueta y el centro
         textprops={'size': 'large'})  # Tamaño de la fuente
 
-plt.title('Distribución de películas y programas de TV en Netflix', fontsize=18)
-plt.axis('equal')  # Garantizar que el gráfico sea circular
+plt.title('Dificultad de preguntas de Leetcode', fontsize=18)
+plt.axis('equal')  # Garantizamos que el gráfico sea circular
 plt.show()
 
 """
-* Analisis:
-El gráfico circular nos muestra que Netflix tiene una oferta más amplia de 
-películas que de programas de TV.
+________________________________________________________________________________
+Generamos un gráfico de pastel en el cual se visualizala distribución de las 
+preguntas de Leetcode por nivel de dificultad (Fácil, Medio, Difícil). El dataset 
+incluye una columna llamada Difficulty,que contiene el nivel de cada pregunta.
+_________________________________________________________________________________
 """
